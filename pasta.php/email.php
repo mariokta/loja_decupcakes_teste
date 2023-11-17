e5e0c2f1fae14f040767f0089a07d4cdda5675ba
mariokta <91229070+mariokta@users.noreply.github.com>
2023-11-17 00:38:48 -0300

    Add files via upload

--- pasta.php/email.php
+++ pasta.php/email.php
@@ -1,27 +1,22 @@
 <?php
-if(issset($_POST['email']) && !empty($_POST['email']))
+if(isset($_POST['email']) && !empty($_POST['email'])) {
+    $nome = addcslashes($_POST['name']);
+    $email = addcslashes($_POST['email']);
+    $mensagem = addcslashes($_POST['message']);
 
-$nome = addcslashes($_POST['name']);
-$email = addcslashes($_POST['email']);
-$mensagem = addcslashes($_POST['message']);
+    $to = "mariokta01@gmail.com";
+    $subject = "Contato - Loja de cupcakes";
+    $body = "Nome: " . $nome . "\n" .
+            "E-mail: " . $email . "\n" .   
+            "Mensagem: " . $mensagem;
+    $header = "from: mariokta01@gmail.com" . "\r\n" .
+            "Reply-to:" . $email . "\r\n" . // Correção aqui
+            "X-Mailer: PHP/" . phpversion(); // Correção aqui
 
-$to = "mariokta01@gmail.com";
-$subject = "Contato - Loja de cupcakes";
-$body = "Nome: " .$nome. "\n".
-        "E-mail: " .$email. "\n".   
-        "Mensagem: " .$mensagem;
-$header = "from: mariokta01@gmail.com"."\r\n"
-            ."Replay-to:".$email."\e\n"
-            ."X=mailer:PHP/".phpversion();
-
-if(mail($to,$subject,$body,$header)){
-
-    echo("Email enviado com sucesso");
+    if(mail($to, $subject, $body, $header)) {
+        echo "Email enviado com sucesso";
+    } else {
+        echo "O Email não pôde ser enviado";
+    }
 }
-
-else{
-    echo("O Email não pode ser enviado");
-}
-
-
 ?>
